31802fb85f1239ed6474efea6d8c6ee688dfa7b2
yuhosi <yuyuyuhaoshi@163.com>
2018-07-22 23:21:03 +0800

    112 118

---
+++ File/112_PathSum/__init__.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        if sum == root.val and not root.left and not root.right:
+            return True
+        sum -= root.val
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
